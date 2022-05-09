fb1d58a559d1a95eaa1cd9a72fef3d2ba0a0ea63
mstewart <mstewart@example.com>
2022-05-09 11:47:55 -0700

    Ensure json serialization of 'LocalDate'

--- src/main/java/com/msfinance/pbalancer/util/JSONHelper.java
+++ src/main/java/com/msfinance/pbalancer/util/JSONHelper.java
@@ -4,24 +4,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JSONHelper
 {
+    private static final JavaTimeModule jsr310 = new JavaTimeModule();
+
     public static String toJson(final Object o) throws IOException
     {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(jsr310);
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.setSerializationInclusion(Include.NON_NULL);
+        mapper.configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true);
         return mapper.writeValueAsString(o);
     }
 
     public static <T> T fromJson(final String val, final Class<T> c) throws JsonMappingException, JsonProcessingException
     {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.registerModule(jsr310);
         return mapper.readValue(val, c);
     }
 
