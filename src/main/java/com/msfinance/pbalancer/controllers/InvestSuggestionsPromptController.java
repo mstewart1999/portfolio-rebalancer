1d7e7c72b194313b9f49aa7d2ba6051b0d83823c
mstewart <mstewart@example.com>
2022-06-02 10:47:51 -0700

    More work towards invest/withdrawal workflows.

--- src/main/java/com/msfinance/pbalancer/controllers/InvestSuggestionsPromptController.java
+++ src/main/java/com/msfinance/pbalancer/controllers/InvestSuggestionsPromptController.java
@@ -1,7 +1,8 @@
 package com.msfinance.pbalancer.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,15 +12,20 @@ import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import com.msfinance.pbalancer.App;
 import com.msfinance.pbalancer.model.Account;
 import com.msfinance.pbalancer.model.Portfolio;
+import com.msfinance.pbalancer.model.rebalance.TempCash;
+import com.msfinance.pbalancer.util.NumberFormatHelper;
 import com.msfinance.pbalancer.util.Validation;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.RowConstraints;
 
 public class InvestSuggestionsPromptController extends BaseController<Portfolio,Void>
 {
@@ -30,7 +36,7 @@ public class InvestSuggestionsPromptController extends BaseController<Portfolio,
     private Label nameLabel;
 
     @FXML
-    private Pane whichAccountsPane;
+    private GridPane whichAccountsPane;
 
     @FXML
     private Button nextBtn;
@@ -39,9 +45,15 @@ public class InvestSuggestionsPromptController extends BaseController<Portfolio,
     private Button cancelBtn;
 
 
+    private final List<TextField> cashTextFields;
+    private final List<Label> errorLabels;
+
+
     public InvestSuggestionsPromptController()
     {
         super(null);
+        cashTextFields = new ArrayList<>();
+        errorLabels = new ArrayList<>();
     }
 
     @FXML
@@ -51,6 +63,8 @@ public class InvestSuggestionsPromptController extends BaseController<Portfolio,
         Validation.assertNonNull(whichAccountsPane);
         Validation.assertNonNull(nextBtn);
         Validation.assertNonNull(cancelBtn);
+
+        whichAccountsPane.setVgap(4);
 
         ButtonBar.setButtonData(cancelBtn, ButtonData.CANCEL_CLOSE);
         ButtonBar.setButtonData(nextBtn, ButtonData.NEXT_FORWARD);
@@ -77,6 +91,31 @@ public class InvestSuggestionsPromptController extends BaseController<Portfolio,
     protected void populateData(final Portfolio p)
     {
         nameLabel.setText(p.getName());
+
+        whichAccountsPane.getChildren().clear();
+        whichAccountsPane.getRowConstraints().clear();
+        cashTextFields.clear();
+        errorLabels.clear();
+
+        int row = 0;
+        for(Account a : p.getAccounts())
+        {
+            Label acctNameLabel = new Label(a.getName());
+            TextField cashText = new TextField();
+            Label errorLabel = new Label();
+            acctNameLabel.setPadding(new Insets(8));
+            cashText.setPadding(new Insets(8));
+            errorLabel.setPadding(new Insets(8));
+            cashTextFields.add(cashText);
+            errorLabels.add(errorLabel);
+
+            whichAccountsPane.add(acctNameLabel, 0, row);
+            whichAccountsPane.add(cashText, 1, row);
+            whichAccountsPane.add(errorLabel, 2, row);
+
+            whichAccountsPane.getRowConstraints().add(new RowConstraints(0, Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE));
+            row++;
+        }
     }
 
     @Override
@@ -101,7 +140,7 @@ public class InvestSuggestionsPromptController extends BaseController<Portfolio,
     {
         if(checkData())
         {
-            getApp().<Map<Account,Double>,Void>mySwitchView(App.INVEST_SUGGESTIONS_RESULTS_VIEW, getData(),
+            getApp().<TempCash,Void>mySwitchView(App.INVEST_SUGGESTIONS_RESULTS_VIEW, getData(),
                     a -> returnSuccess(a),
                     () -> returnFailure());
         }
@@ -109,13 +148,63 @@ public class InvestSuggestionsPromptController extends BaseController<Portfolio,
 
     private boolean checkData()
     {
-        // TODO Auto-generated method stub
-        return false;
+        int valid = 0;
+        int invalid = 0;
+        int row = 0;
+        for(TextField tf : cashTextFields)
+        {
+            errorLabels.get(row).setText("");
+            String valStr = tf.getText().trim();
+            if(!valStr.isBlank())
+            {
+                BigDecimal valNbr = NumberFormatHelper.parseNumber2(valStr);
+                if((valNbr != null) && (valNbr.doubleValue() >= 0.0))
+                {
+                    if(valNbr.doubleValue() >= 0.01)
+                    {
+                        // zeros are not invalid, but only positive values really matter
+                        valid++;
+                    }
+                }
+                else
+                {
+                    // user wrote something in the field, but it either isn't
+                    // a number, or is negative
+                    errorLabels.get(row).setText("Invalid number");
+                    invalid++;
+                }
+            }
+            row++;
+        }
+        return (valid > 0) && (invalid == 0);
     }
 
-    private Map<Account,Double> getData()
+    private TempCash getData()
     {
-        // TODO Auto-generated method stub
-        return new HashMap<>();
+        List<Account> accounts = getIn().getAccounts();
+        TempCash out = new TempCash();
+        int row = 0;
+        for(TextField tf : cashTextFields)
+        {
+            String valStr = tf.getText().trim();
+            if(!valStr.isBlank())
+            {
+                BigDecimal valNbr = NumberFormatHelper.parseNumber2(valStr);
+                if((valNbr != null) && (valNbr.doubleValue() >= 0.0))
+                {
+                    if(valNbr.doubleValue() >= 0.01)
+                    {
+                        // zeros are not invalid, but only positive values really matter
+                        out.add(accounts.get(row), valNbr.doubleValue());
+                    }
+                }
+                else
+                {
+                    // ignore, assume this was caught by checkData()
+                }
+            }
+            row++;
+        }
+        return out;
     }
 }
