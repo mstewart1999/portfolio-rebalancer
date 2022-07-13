05c3a1eedb711bdc875e22356cf8a3d636fb5a80
mstewart <mstewart@example.com>
2022-07-13 09:51:50 -0700

    Add additional institutions

--- src/main/java/com/msfinance/pbalancer/model/Institution.java
+++ src/main/java/com/msfinance/pbalancer/model/Institution.java
@@ -4,14 +4,42 @@ import java.util.List;
 
 public class Institution
 {
-    // TODO: get a better list
     public static List<String> ALL = List.of(
-            "Vanguard",
-            "Fidelity",
-            "Schwab",
-            "TD Ameritrade",
-            "E*Trade",
+            "Allianz",
+            "American Century",
+            "American Funds",
+            "Ameriprise Financial",
+            "Bank of America",
+            "Berwyn Funds",
+            "Charles Schwab",
+            "Dodge and Cox",
+            "Dreyfus Investment",
+            "DWS Scudder",
+            "E*Trade Financial",
+            "Edward Jones",
+            "Federal Government",
+            "Fidelity Investments",
+            "Franklin Templeton Investments",
+            "Invesco Funds (for former AIM Funds)",
+            "Janus Investments",
+            "JP Morgan",
+            "Legg Mason",
+            "Merrill Lynch",
+            "Morgan Stanley Dean Witter MSDW",
+            "Prudential Financial",
+            "Putnam Investments",
+            "Scottrade",
+            "Smith Barney",
+            "T.RowePrice",
             "Treasury Direct",
+            "TD Ameritrade",
+            "TIAA-CREF",
+            "UBS Wealth Management",
+            "USAA",
+            "Vanguard",
+            "Wachovia Bank",
+            "Wells Fargo Advantage Funds",
+
             "Other"
             );
 
