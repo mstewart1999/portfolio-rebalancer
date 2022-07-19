452bcb5fe4d4d2ef502ed247b3256b7b4ff5a3be
mstewart <mstewart@example.com>
2022-07-19 16:45:29 -0700

    Some setup for native executable.

---
+++ src/main/java/com/msfinance/pbalancer/ConsoleSubstitute.java
@@ -0,0 +1,14 @@
+package com.msfinance.pbalancer;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class ConsoleSubstitute extends PrintStream
+{
+    public ConsoleSubstitute(final String filename) throws FileNotFoundException
+    {
+        super(new FileOutputStream(filename), true, StandardCharsets.UTF_8);
+    }
+}
---
+++ win-build-native.cmd
@@ -0,0 +1,17 @@
+
+REM https://medium.com/graalvm/using-graalvm-and-native-image-on-windows-10-9954dc071311
+REM https://docs.gluonhq.com/#platforms_windows
+
+set GRAALVM_HOME=C:\tools\graalvm-svm-java17-windows-gluon-22.1.0.1-Final
+set JAVA_HOME=C:\tools\graalvm-svm-java17-windows-gluon-22.1.0.1-Final
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+if not defined VSCMD_VER (
+  call C:\tools\MicrosoftVisualStudio\2022\Community\VC\Auxiliary\Build\vcvars64.bat
+)
+
+
+call .\gradlew nativeBuild nativePackage --stacktrace
+
+rem editbin /SUBSYSTEM:CONSOLE .\build\gluonfx\x86_64-windows\pbalancerApp.exe
+
